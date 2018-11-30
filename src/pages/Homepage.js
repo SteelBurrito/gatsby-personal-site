b60aff1350b5b1395c722606e84c82ee6b339ebc
steelburrito <antouniokevin@gmail.com>
2018-11-30 13:55:11 +1100

    added animation when page is mounted

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,13 +1,27 @@
 import React from "react";
 import "../styles/Homepage.css";
+import { CSSTransitionGroup } from "react-transition-group";
 
 class Homepage extends React.Component {
+  componentDidMount() {}
+
   render() {
-    return (
+    const heading = (
       <div className="heading">
         <h1>kevdev.online</h1>
         <p>Personal page by Antonio Kevin Christophorus</p>
       </div>
+    );
+    return (
+      <CSSTransitionGroup
+        transitionName="fadein"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}
+      >
+        {heading}
+      </CSSTransitionGroup>
     );
   }
 }
